e8d20f0f8775764ac2744e2777801e1ba06026cc
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-10-09 19:36:14 +0200

    added some features to select a color from given colors

--- colorGame.js
+++ colorGame.js
@@ -8,8 +8,37 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[6];
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 for(var i = 0; i < squares.length; i++) {
   squares[i].style.backgroundColor = colors[i];
+  squares[i].addEventListener("click", function() {
+    //grab color of clicked square
+    var clickedColor = this.style.backgroundColor;
+    //compare color to pickedColor
+    if(clickedColor === pickedColor) {
+      messageDisplay.textContent = "Correct"
+      changeColors(clickedColor);
+    } else {
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again"
+    }
+  });
+}
+
+colorDisplay.textContent = pickedColor;
+
+function changeColors(color) {
+  //loop through all squares
+  for(var i = 0; i < squares.length; i++) {
+    //change each color to match given color
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
