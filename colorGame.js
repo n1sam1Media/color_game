a6d7a6f84302826b0acafd4ad8a83f86bf781b1f
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-10-11 20:33:38 +0200

    added some more features to the buttons

--- colorGame.js
+++ colorGame.js
@@ -1,5 +1,5 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -67,7 +67,7 @@ function randomColor() {
 
 resetButton.addEventListener("click", function() {
   //generate all new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   //pick a new random color from array
   pickedColor = pickColor();
   //change colorDisplay to match picked color
@@ -76,17 +76,20 @@ resetButton.addEventListener("click", function() {
   for(var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
   }
-  h1.style.backgroundColor = "#232323";
+  h1.style.backgroundColor = "steelblue";
   this.textContent = "New Colors";
   messageDisplay.textContent = "";
 });
 
 easyBtn.addEventListener("click", function() {
+  h1.style.backgroundColor = "steelblue";
   hardBtn.classList.remove("selected");
   easyBtn.classList.add("selected");
-  colors = generateRandomColors(3);
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
   pickedColor = pickColor();
   colorDisplay.textContent = pickedColor;
+  messageDisplay.textContent = "";
 
   //disappear 3 colors
   for(var i = 0; i < squares.length; i++) {
@@ -99,6 +102,17 @@ easyBtn.addEventListener("click", function() {
 });
 
 hardBtn.addEventListener("click", function() {
+  h1.style.backgroundColor = "steelblue";
   easyBtn.classList.remove("selected");
   hardBtn.classList.add("selected");
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  messageDisplay.textContent = "";
+  
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = "block";
+  }
 });
